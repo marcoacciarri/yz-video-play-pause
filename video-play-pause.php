9b9938fa01a6bc298c51f640f779bcb9e98f8c6d
Marco <marco.acciarri@yourzone.it>
2023-10-07 08:12:26 +0200

    update version

--- video-play-pause.php
+++ video-play-pause.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://yourzone.website
- * @since             1.0.1
+ * @since             1.0.2
  * @package           Video_Play_Pause
  *
  * @wordpress-plugin
  * Plugin Name:       Vimeo Play Pause
  * Plugin URI:        https://yourzone.website
  * Description:       Plays and Pauses videos only when in view.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Marco Acciarri
  * Author URI:        https://yourzone.website/
  * License:           GPL-2.0+
